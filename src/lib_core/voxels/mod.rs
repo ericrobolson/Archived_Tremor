81c5abdf76142ae47fda29a42d1b6072a1218b52
Eric Olson <eric.rob.olson@gmail.com>
2020-10-13 18:04:28 -0700

    Started fleshing out texture generation.

--- src/lib_core/voxels/mod.rs
+++ src/lib_core/voxels/mod.rs
@@ -11,15 +11,110 @@ const CHILD_POINTER:    u32 = 0b1111_1111_1111_1110_0000_0000_0000_0000;
 #[cfg_attr(rustfmt, rustfmt_skip)]
 const VALID_MASK:       u32 = 0b0000_0000_0000_0000_0000_0000_1111_1111;
 #[cfg_attr(rustfmt, rustfmt_skip)]
+const VALID_MASK_INDEX: u32 = 0b0000_0000_0000_0000_0000_0000_1000_0000;
+
+#[cfg_attr(rustfmt, rustfmt_skip)]
 const LEAF_MASK:        u32 = 0b0000_0000_0000_0000_1111_1111_0000_0000;
+#[cfg_attr(rustfmt, rustfmt_skip)]
+const LEAF_MASK_INDEX:  u32 = 0b0000_0000_0000_0000_1000_0000_0000_0000;
 
-pub struct VoxelStreamManager {}
+#[derive(Copy, Clone, PartialEq)]
+pub enum VoxelStreamTypes {
+    PageHeader(u32),
+    Voxel(Octree),
+    InfoSection,
+}
 
-pub struct Voxel {
+pub struct VoxelStreamManager {
+    next_page_header: u32,
+    data: Vec<VoxelStreamTypes>,
+}
+
+impl VoxelStreamManager {
+    pub fn new() -> Self {
+        let mut stream_manager = Self {
+            next_page_header: 0,
+            data: vec![], // TODO: reserve data?
+        };
+
+        // TODO: automate adding of headers when adding/removing voxels
+        stream_manager.add_header();
+
+        // Add a single voxel/octree node.
+        let mut voxel = Octree::empty();
+        voxel.add_child(0);
+        voxel.add_child(7);
+        stream_manager.data.push(VoxelStreamTypes::Voxel(voxel));
+
+        stream_manager
+    }
+
+    fn add_header(&mut self) -> u32 {
+        let page_header = self.next_page_header;
+        self.data.push(VoxelStreamTypes::PageHeader(page_header));
+        self.next_page_header += 1;
+
+        page_header
+    }
+
+    fn texture(&self, device: &wgpu::Device, queue: &wgpu::Queue) {
+        // Could probably use some optimization
+        let bytes: Vec<u8> = self
+            .data
+            .iter()
+            .map(|d| match d {
+                VoxelStreamTypes::PageHeader(header) => *header,
+                VoxelStreamTypes::Voxel(octree) => octree.mask,
+                VoxelStreamTypes::InfoSection => 0,
+            })
+            .map(|b| b.to_le_bytes())
+            .collect::<Vec<[u8; 4]>>()
+            .iter()
+            .map(|b| b.iter())
+            .flatten()
+            .map(|b| *b)
+            .collect();
+
+        let tex_size = wgpu::Extent3d {
+            width: self.data.len() as u32,
+            height: 1,
+            depth: 1,
+        };
+        let texture = device.create_texture(&wgpu::TextureDescriptor {
+            size: tex_size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D1,
+            format: wgpu::TextureFormat::R32Uint,
+            usage: wgpu::TextureUsage::STORAGE | wgpu::TextureUsage::COPY_DST, // TODO: wire up to shader for storage (https://docs.rs/wgpu/0.6.0/wgpu/enum.BindingType.html#variant.StorageTexture)
+            label: Some("voxels"),
+        });
+
+        queue.write_texture(
+            wgpu::TextureCopyView {
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            &bytes, // bytes?
+            wgpu::TextureDataLayout {
+                offset: 0,
+                bytes_per_row: bytes.len() as u32,
+                rows_per_image: 1,
+            },
+            tex_size,
+        );
+
+        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+    }
+}
+
+#[derive(Copy, Clone, PartialEq)]
+pub struct Octree {
     mask: u32,
 }
 
-impl Voxel {
+impl Octree {
     pub fn serialize(&self) -> [u8; 4] {
         self.mask.to_le_bytes()
     }
@@ -28,6 +123,14 @@ impl Voxel {
         Self {
             mask: u32::from_le_bytes(bytes),
         }
+    }
+
+    pub fn empty() -> Self {
+        Self { mask: 0 }
+    }
+
+    pub fn from_u32(voxel: u32) -> Self {
+        Self { mask: voxel }
     }
 
     pub fn child_pointer(&self) -> u16 {
@@ -75,5 +178,16 @@ impl Voxel {
     pub fn is_leaf_voxel(&self, child: u8) -> bool {
         // if both bits set, contains a leaf voxel
         self.valid_child(child) && self.leaf_child(child)
+    }
+
+    pub fn add_child(&mut self, child: u8) {
+        // Simple dummy code to add leafs
+        let child_index = child % 8;
+
+        let leaf_mask = LEAF_MASK_INDEX >> child_index;
+        let valid_mask = VALID_MASK_INDEX >> child_index;
+
+        self.mask |= leaf_mask;
+        self.mask |= valid_mask;
     }
 }
