17f5551d5a270ef706dc88c3ce9133ad80926d5b
Eric Olson <eric.rob.olson@gmail.com>
2020-10-26 09:25:16 -0700

    Added drawing for individual chunks.

--- src/gfx/voxels.rs
+++ src/gfx/voxels.rs
@@ -6,23 +6,46 @@ use crate::lib_core::{
 };
 
 pub struct VoxelPass {
-    pub buffer: wgpu::Buffer,
-    pub vert_len: usize,
     meshes: Vec<Mesh>,
 }
 
 impl VoxelPass {
     pub fn new(device: &wgpu::Device) -> Self {
+        //TODO: move to ecs
         let size = 4;
         let chunk_manager = ChunkManager::new(size, size, size);
-        let verts: Vec<VoxelChunkVertex> = chunk_manager
-            .meshes
-            .iter()
-            .map(|c| VoxelChunkVertex::from_chunk(&c))
-            .flatten()
-            .collect();
 
-        let meshes = Vec::with_capacity(chunk_manager.len());
+        let mut meshes = Vec::with_capacity(chunk_manager.len());
+        for i in 0..chunk_manager.meshes.len() {
+            let mesh = Mesh::new(&chunk_manager.meshes[i], device);
+            meshes.push(mesh);
+        }
+
+        Self { meshes }
+    }
+
+    pub fn update(&mut self, world: &World) {
+        // TODO: update each chunk if changed
+    }
+
+    pub fn draw<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
+        // Draw each chunk.
+        // TODO: frustrum culling
+        for mesh in &self.meshes {
+            mesh.draw(render_pass);
+        }
+    }
+}
+
+struct Mesh {
+    last_updated: GameFrame,
+    vert_len: usize,
+    buffer: wgpu::Buffer,
+}
+
+impl Mesh {
+    fn new(chunk: &ChunkMesh, device: &wgpu::Device) -> Self {
+        let verts: Vec<VoxelChunkVertex> = VoxelChunkVertex::from_chunk(chunk);
 
         use wgpu::util::DeviceExt;
         let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
@@ -32,36 +55,20 @@ impl VoxelPass {
         });
 
         Self {
-            buffer,
             vert_len: verts.len(),
-            meshes,
+            buffer,
+            last_updated: 0,
         }
-    }
-
-    pub fn update(&mut self, world: &World) {
-        // TODO: update each chunk
-    }
-
-    pub fn draw<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
-        // TODO: change to use each individual chunk
-        render_pass.set_vertex_buffer(0, self.buffer.slice(..));
-        render_pass.draw(0..self.vert_len as u32, 0..1);
-    }
-}
-
-struct Mesh {
-    last_updated: GameFrame,
-    verts: Vec<VoxelChunkVertex>,
-}
-
-impl Mesh {
-    fn new() -> Self {
-        unimplemented!();
     }
 
     fn update(&mut self, chunk: &Chunk) {
         if self.last_updated < chunk.last_update() {
             // TODO: remesh
         }
+    }
+
+    fn draw<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
+        render_pass.set_vertex_buffer(0, self.buffer.slice(..));
+        render_pass.draw(0..self.vert_len as u32, 0..1);
     }
 }
